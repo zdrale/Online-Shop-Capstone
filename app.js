d4f20d008153c505cc232d044d91977ec8b26ab0
zdrale <nikolazd22@outlook.com>
2020-10-13 10:30:51 +0200

    Added screenshots

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
-$('.catalogue-wrapper').slick({
+$('.catalogue-wrapper').slick({ 
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 3,
   prevArrow: false,
-  nextArrow: false
+  nextArrow: false 
 });
