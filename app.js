c5b595ca0cb34c3d8d5761c83206a58cb7e1551b
zdrale <nikolazd22@outlook.com>
2020-10-15 12:40:12 +0200

    Updated Mens and Womens Sectio
    -Changed background color to match original one

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ $('.catalogue-wrapper').slick({
   prevArrow: false,
   nextArrow: false 
 });
+
+$('.catalogue-wrapper-hot').slick({ 
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow: false,
+  nextArrow: false 
+});
